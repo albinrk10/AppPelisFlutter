0e568b6cc4888698bdaf783935153da12727b0f2
albinrk10 <albinrk@gmail.com>
2023-02-05 21:41:05 -0500

    agregado de nuevas archivos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,36 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas_albin/widgets/widgets.dart';
 
-
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Peliculas en cines'),
+        title: Text('Peliculas en cines'),
         elevation: 0,
         actions: [
           IconButton(
             icon: Icon(Icons.search_outlined),
-            onPressed: (() {
-              
-            }),
-             )
+            onPressed: (() {}),
+          )
         ],
-        
       ),
-      body:Column(
+      body: SingleChildScrollView(
+          child: Column(
         children: [
-          //TODO: cardSwupe
-          CardSwiper()
+          //Tarjetas principales
+          CardSwiper(),
 
-          //listado de peliculas
-
+          //slider de peliculas
+          MovieSlider(),
         ],
-      )
+      )),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:peliculas_albin/widgets/card_swiper.dart';
-
+export 'package:peliculas_albin/widgets/casting_cards.dart';
+export 'package:peliculas_albin/widgets/movie_slider.dart';
